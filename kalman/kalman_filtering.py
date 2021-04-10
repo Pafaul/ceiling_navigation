289b796f1d5bd4028509bc7108220a81f4071a0f
NikolayShvets <kolya.shvets20@gmail.com>
2021-04-10 21:22:27 +0300

    Апдейт ФК

--- kalman/kalman_filtering.py
+++ kalman/kalman_filtering.py
@@ -1,49 +1,84 @@
 import numpy as np
+from filterpy import kalman
+from filterpy.common import Q_discrete_white_noise
+from typing import Tuple, List
 
 
 class KalmanFiltering:
     def __init__(self, x: np.array,
-                 z: np.array,
-                 r: np.array,
-                 en: np.array,
-                 d_noise: np.array):
+                 dim_z: int,
+                 time_step: float,
+                 p_x: np.array,
+                 r_x: np.array,
+                 process_noise: float = 1e-3):
         """
-        Фильтрация Калмана как функция с состоянием.
-        Каждая следующая итерация происходит по вызову callable объекта.
+        Фильтр Калмана как callable объект. Возвращает новую оценку вектора состояния по вызову __call__.
+        :param x: Начальный вектор состояния.
+        :param dim_z: Длина вектора наблюдений.
+        :param p_x: Дисперсии начального вектора состояния.
+        :param r_x: Шумы измерений.
+        :param process_noise: Погрешность модели.
+        :param time_step: шаг времени.
+        """
 
-        :param x: Начальный вектор состояния
-        :param z: Начальный вектор наблюдений
-        :param r: Вектор корреляций между координатами вектора состояний
-        :param en: Вектор возмущений (возмущения нормальные: M = 0; дисперсии координат некоррелированы)
-        :param d_noise: Вектор ошибок наблюдений (ошибки нормальные: M = 0; дисперсии координат некоррелированы)
-        """
         self.__x = x
-        self.__z = z
-        self.__r = np.diag(r)
-        self.__v_ksi = np.diag(en)
-        self.__v = np.diag(d_noise)
-        self.__v_inv = np.linalg.inv(self.__v)
-        self.__observation_number = 1
-        # формирование оценки перемещения и вектора дисперсий ошибок этой оценки на первый шаг
-        self.__xx, self.__p = self.__setup()
+        self.__p_x = p_x
+        self.__r_x = r_x
+        self.__process_noise = process_noise
+        self.__dim_z = dim_z
+        self.__time_step = time_step
+        self.__filter = kalman.KalmanFilter(dim_x=len(self.__x),
+                                            dim_z=self.__dim_z)
+        # Хранилище векторов оценок состояния
+        self.__filtered_state = list()
+        # Хранилище квадратов ошибок оценки состояния
+        self.__covariance_history = list()
 
-    def __setup(self):
-        # вектор оценок перемещения
-        xx = np.zeros(self.__observation_number * len(self.__x)) \
-            .reshape(self.__observation_number, len(self.__x))
-        # вектор дисперсий ошибок оценивания
-        p = self.__v
-        # первая оценка
-        xx[:][0] = self.__z
-        return xx, p
+    @property
+    def filtered_state(self) -> List[np.array]:
+        return self.__filtered_state
 
-    def __call__(self, z: np.array, d_noise: np.array, **kwargs):
-        self.__v = np.diag(d_noise)
-        self.__v_inv = np.linalg.inv(self.__v)
-        pe = np.dot(np.dot(self.__r, self.__p), self.__r.T) + self.__v_ksi
-        self.__p = np.dot(pe, self.__v) * np.linalg.inv(pe + self.__v)
-        xe = np.dot(self.__r, self.__xx[:][self.__observation_number - 1])
-        iteration_result = self.__xx[:][self.__observation_number] = xe + np.dot(np.dot(self.__p, self.__v_inv),
-                                                                                 (z - xe))
-        self.__observation_number += 1
-        return self.__p, iteration_result
+    @property
+    def covariance_history(self) -> List[np.array]:
+        return self.__covariance_history
+
+    def setup(self) -> None:
+        """
+        Инициализация параметров фильтра Калмана.
+        :return: None.
+        """
+
+        # Матрица процесса (ДУ движения)
+        self.__filter.F = np.array([[1, 0, self.__time_step, 0, 0, 0, 0, 0],
+                                    [0, 1, 0, self.__time_step, 0, 0, 0, 0],
+                                    [0, 0, 1, 0, 0, 0, 0, 0],
+                                    [0, 0, 0, 1, 0, 0, 0, 0],
+                                    [0, 0, 0, 0, 1, 0, self.__time_step, 0],
+                                    [0, 0, 0, 0, 0, 1, 0, self.__time_step],
+                                    [0, 0, 0, 0, 0, 0, 1, 0],
+                                    [0, 0, 0, 0, 0, 0, 0, 1]])
+        # Матрица-связь вектора состояния с показаниями датчиков
+        self.__filter.H = np.eye(len(self.__x))
+        # Ковариационная матрица ошибки модели
+        self.__filter.Q = Q_discrete_white_noise(dim=len(self.__x), dt=self.__time_step, var=self.__process_noise)
+        # Ковариационная матрица ошибки измерений
+        self.__filter.R = np.diag(self.__r_x)
+        # Ковариационная матрица состояния
+        self.__filter.P = np.diag(self.__p_x)
+
+    def __call__(self, z: np.array, time_step: float) -> Tuple[np.array, np.array]:
+        """
+        Производит очерендую итерацию оценки вектора состояния по переданному вектору наблюдений.
+        :param z: Вектор наблюдений.
+        :param time_step: Шаг времени.
+        :return: Кортеж вида (Последняя оценка вектора состояния, ковариационная матрица ошибок этой оценки).
+        """
+
+        # Пересчитаем ковариационную матрицу ошибки модели, т.к шаг времени мог измениться
+        self.__filter.Q = Q_discrete_white_noise(dim=len(self.__x), dt=time_step, var=self.__process_noise)
+
+        self.__filter.predict()
+        self.__filter.update(z)
+        self.__filtered_state.append(self.__filter.x)
+        self.__covariance_history.append(self.__filter.P)
+        return self.__filtered_state[-1], self.__covariance_history[-1]
--- main.py
+++ main.py
@@ -13,10 +13,9 @@ from camera_utils.camera_calibration import calibrate_camera
 from camera_utils.get_position_deltas import get_position_deltas
 from kalman import KalmanFiltering
 
-
 InitialStageInfo = TypeVar('InitialStageInfo', np.array, np.array, cv2.VideoCapture)
-MultipleDicts    = TypeVar('MultipleDicts', dict, dict)
-MultipleArrays   = TypeVar('MultipleArrays', np.array, np.array)
+MultipleDicts = TypeVar('MultipleDicts', dict, dict)
+MultipleArrays = TypeVar('MultipleArrays', np.array, np.array)
 
 
 def main():
@@ -24,7 +23,7 @@ def main():
     config, debug = get_config(filename)
     camera_matrix, dist, camera = initial_stage(config)
     descriptor, matcher = initialize_algorithms(config)
-    
+
     tmp_img_current = None
     tmp_des_current, tmp_des_previous = None, None
     tmp_kp_current, tmp_kp_previous = None, None
@@ -40,20 +39,21 @@ def main():
         while current_time < finish_time:
             res, tmp_img_current = camera.read()
             if not res:
-                print('Cannot get image from camera')   
+                print('Cannot get image from camera')
                 continue
-            
+
             start_time = time.time()
             tmp_kp_current, tmp_des_current = get_keypoints_from_image(tmp_img_current, descriptor)
 
             if tmp_kp_previous is not None and tmp_kp_current is not None:
-                tmp_pts_current, tmp_pts_previous = match_keypoints(tmp_kp_current, tmp_des_current, tmp_kp_previous, tmp_des_previous)
+                tmp_pts_current, tmp_pts_previous = match_keypoints(tmp_kp_current, tmp_des_current, tmp_kp_previous,
+                                                                    tmp_des_previous)
                 R, t = get_position_deltas(tmp_pts_previous, tmp_pts_current, camera_matrix)
                 print(R)
                 print(t)
                 delta_R.append(R.copy())
                 delta_t.append(t.copy())
-            
+
             tmp_kp_previous = tmp_kp_current.copy()
             tmp_des_previous = tmp_des_current.copy()
             delta = time.time() - start_time
@@ -66,19 +66,23 @@ def get_config(filename: str) -> MultipleDicts:
     config.read(filename)
     return config, config['debug']
 
+
 def initial_stage(config: dict) -> InitialStageInfo:
     mtx, dist = get_camera_matrix(config['camera_matrix'])
     camera = get_camera(config['camera'])
     return [mtx, dist, camera]
 
+
 def initialize_algorithms(config: dict) -> Any:
     return cv2.SIFT_create(contrastThreshold=0.1), cv2.BFMatcher()
+
 
 def get_camera_matrix(config: dict) -> list[np.array]:
     if config['calibration_image'] != '':
         return calibrate_camera(calibration_image=config['calibration_image'])
     else:
         return calibrate_camera(calibration_file=config['calibration_file'])
+
 
 def get_camera(config: dict) -> cv2.VideoCapture:
     if config['source'] != '':
@@ -91,31 +95,30 @@ def get_camera(config: dict) -> cv2.VideoCapture:
 def not_main():
     img1 = cv2.imread('./images/2.jpg', 0)
     img2 = cv2.imread('./images/1.jpg', 0)
-    #orb = cv2.ORB_create()
+    # orb = cv2.ORB_create()
     orb = cv2.SIFT_create(contrastThreshold=0.1)
-    #orb = cv2.SIFT_create()
+    # orb = cv2.SIFT_create()
 
-    
     kp1, des1 = orb.detectAndCompute(img1, None)
     start_time = time.time()
     kp2, des2 = orb.detectAndCompute(img2, None)
 
     # BFMatcher with default params
     bf = cv2.BFMatcher()
-    matches = bf.knnMatch(des1,des2,k=2)
-    
+    matches = bf.knnMatch(des1, des2, k=2)
+
     pts1 = []
     pts2 = []
-    for i,(m,n) in enumerate(matches):
-        if m.distance < 0.8*n.distance:
+    for i, (m, n) in enumerate(matches):
+        if m.distance < 0.8 * n.distance:
             pts2.append(kp2[m.trainIdx].pt)
             pts1.append(kp1[m.queryIdx].pt)
 
     pts1 = np.int32(pts1)
     pts2 = np.int32(pts2)
 
-    F, mask = cv2.findFundamentalMat(pts1,pts2,cv2.FM_LMEDS)
-    
+    F, mask = cv2.findFundamentalMat(pts1, pts2, cv2.FM_LMEDS)
+
     camera_matrix = np.array([
         [2.20577589e+03, 0.00000000e+00, 4.01509158e+02],
         [0.00000000e+00, 2.73065460e+03, 3.01560236e+02],
@@ -128,11 +131,10 @@ def not_main():
 
     X, mask, error_EV, error_DP = seq_lsm(pts1, pts2, R)
 
-
     pts1 = np.int32(pts1[mask])
     pts2 = np.int32(pts2[mask])
 
-    F, mask = cv2.findFundamentalMat(pts1,pts2,cv2.FM_LMEDS)
+    F, mask = cv2.findFundamentalMat(pts1, pts2, cv2.FM_LMEDS)
     E, mask = cv2.findEssentialMat(pts1, pts2, camera_matrix, method=cv2.RANSAC)
     R1, R2, t = cv2.decomposeEssentialMat(E)
     retval, R, t, mask = cv2.recoverPose(E, pts1, pts2, camera_matrix, mask=mask)
@@ -140,9 +142,9 @@ def not_main():
     X, mask, error_EV, error_DP = seq_lsm(pts1, pts2, R)
 
     angle_x = atan2(R[2][1], R[2][2])
-    angle_y = atan2(-R[2][0], (R[2][1]**2 + R[2][2]**2)**0.5)
+    angle_y = atan2(-R[2][0], (R[2][1] ** 2 + R[2][2] ** 2) ** 0.5)
     angle_z = atan2(R[1][0], R[0][0])
-    toDeg = lambda x: x*180/pi
+    toDeg = lambda x: x * 180 / pi
     delta = time.time() - start_time
     print(f'time of execution: {delta}')
     print(f'delta in pixels: {X}')
