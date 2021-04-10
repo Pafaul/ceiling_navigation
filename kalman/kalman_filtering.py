0ebc490c55905695702e86aa76851482f6d95fd3
NikolayShvets <kolya.shvets20@gmail.com>
2021-04-10 22:39:14 +0300

    Добавлен пересчет вектора z

--- kalman/kalman_filtering.py
+++ kalman/kalman_filtering.py
@@ -35,6 +35,7 @@ class KalmanFiltering:
                                             dim_z=self.__dim_z)
         # Хранилище векторов оценок состояния
         self.__filtered_state = list()
+        self.__filtered_state.append(self.__x)
         # Хранилище квадратов ошибок оценки состояния
         self.__covariance_history = list()
 
@@ -46,41 +47,71 @@ class KalmanFiltering:
     def covariance_history(self) -> List[np.array]:
         return self.__covariance_history
 
-    def setup(self) -> None:
+    def __setup(self) -> None:
         """
         Инициализация параметров фильтра Калмана.
         :return: None.
         """
-
         # Матрица процесса (ДУ движения)
-        self.__filter.F = np.array([[1, 0, self.__time_step, 0, 0, 0, 0, 0],
-                                    [0, 1, 0, self.__time_step, 0, 0, 0, 0],
-                                    [0, 0, 1, 0, 0, 0, 0, 0],
-                                    [0, 0, 0, 1, 0, 0, 0, 0],
-                                    [0, 0, 0, 0, 1, 0, self.__time_step, 0],
-                                    [0, 0, 0, 0, 0, 1, 0, self.__time_step],
-                                    [0, 0, 0, 0, 0, 0, 1, 0],
-                                    [0, 0, 0, 0, 0, 0, 0, 1]])
+        # self.__filter.F = np.array([[1, 0, 0, self.__time_step, 0, 0, 0, 0, 0, 0, 0, 0],
+        #                             [0, 1, 0, 0, self.__time_step, 0, 0, 0, 0, 0, 0, 0],
+        #                             [0, 0, 1, 0, 0, self.__time_step, 0, 0, 0, 0, 0, 0],
+        #                             [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        #                             [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+        #                             [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+        #                             [0, 0, 0, 0, 0, 0, 1, 0, 0, self.__time_step, 0, 0],
+        #                             [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, self.__time_step, 0],
+        #                             [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, self.__time_step],
+        #                             [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+        #                             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+        #                             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]])
         # Матрица-связь вектора состояния с показаниями датчиков
         self.__filter.H = np.eye(len(self.__x))
         # Ковариационная матрица ошибки модели
-        self.__filter.Q = Q_discrete_white_noise(dim=len(self.__x), dt=self.__time_step, var=self.__process_noise)
+        # self.__filter.Q = Q_discrete_white_noise(dim=len(self.__x), dt=self.__time_step, var=self.__process_noise)
         # Ковариационная матрица ошибки измерений
         self.__filter.R = np.diag(self.__r_x)
         # Ковариационная матрица состояния
         self.__filter.P = np.diag(self.__p_x)
 
-    def __call__(self, z: np.array, time_step: float) -> Tuple[np.array, np.array]:
+    def __compute_z(self, deltas: np.array):
+        return np.array([self.__filtered_state[-1][0] + deltas[0],
+                         self.__filtered_state[-1][1] + deltas[1],
+                         self.__filtered_state[-1][2] + deltas[2],
+                         deltas[3] / deltas[-1],
+                         deltas[4] / deltas[-1],
+                         deltas[5] / deltas[-1],
+                         self.__filtered_state[-1][6] + deltas[6],
+                         self.__filtered_state[-1][7] + deltas[7],
+                         self.__filtered_state[-1][8] + deltas[8],
+                         deltas[9] / deltas[-1],
+                         deltas[10] / deltas[-1],
+                         deltas[11] / deltas[-1]]), deltas[-1]
+
+    def __call__(self, deltas: np.array) -> Tuple[np.array, np.array]:
         """
-        Производит очерендую итерацию оценки вектора состояния по переданному вектору наблюдений.
-        :param z: Вектор наблюдений.
-        :param time_step: Шаг времени.
+        Производит очередную итерацию оценки вектора состояния по переданному вектору наблюдений.
+        :param deltas: Сырой вектор наблюдений.
         :return: Кортеж вида (Последняя оценка вектора состояния, ковариационная матрица ошибок этой оценки).
         """
 
+        z, time_step = self.__compute_z(deltas=deltas)
         if len(z) != self.__dim_z:
             raise VectorSizeMismatch(f"Передан вектор длиной {len(z)}, "
                                      f"ожидается {self.__dim_z}")
+
+        self.__filter.F = np.array([[1, 0, 0, time_step, 0, 0, 0, 0, 0, 0, 0, 0],
+                                    [0, 1, 0, 0, time_step, 0, 0, 0, 0, 0, 0, 0],
+                                    [0, 0, 1, 0, 0, time_step, 0, 0, 0, 0, 0, 0],
+                                    [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+                                    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+                                    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+                                    [0, 0, 0, 0, 0, 0, 1, 0, 0, time_step, 0, 0],
+                                    [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, time_step, 0],
+                                    [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, time_step],
+                                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+                                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+                                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]])
 
         self.__filter.Q = Q_discrete_white_noise(dim=len(self.__x), dt=time_step, var=self.__process_noise)
 
