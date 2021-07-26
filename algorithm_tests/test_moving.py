f43ff527359c0ee056a7d5355566ece401e9940e
Paul Mikhaylov <paulmihailov2013@gmail.com>
2021-07-26 21:01:16 +0300

    Test of algorithm

--- algorithm_tests/test_moving.py
+++ algorithm_tests/test_moving.py
@@ -1,13 +1,14 @@
 import cv2
 import numpy as np
-from math import pi, atan
+from math import pi, atan, atan2, cos
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 from numpy import sin
 
 from algorithm_tests.camera import Camera
 from algorithm_tests.keypoint_generator import generate_keypoints_equal
-from algorithm_tests.utils import calculate_rotation_matrix, calculate_rotation_matrix_from_euler_angles
+from algorithm_tests.utils import calculate_rotation_matrix, calculate_rotation_matrix_from_euler_angles, \
+    calculate_rotation_matrix_from_pdf, calculate_rotation_matrix_euler_angles
 from algorithm_tests.optical_flow_visualization import visualize_not_moving_coordinates, \
     visualize_keypoints, visualize_camera_fov, visualize_keypoints_without_transformation, visualize_keypoints_deltas
 from lsm.least_square_method import seq_lsm
@@ -21,12 +22,46 @@ def rad_to_deg(x: float) -> float:
     return x*180/pi
 
 
+# def calculate_angles_from_rotation_matrix(R: np.ndarray) -> list:
+#     angle_x = atan2(R[1,2], R[2, 2])
+#     c2 = (R[0, 0]**2 + R[0, 1]**2)**0.5
+#     angle_y = atan2(-R[0, 2], c2)
+#     s1 = sin(angle_x)
+#     c1 = cos(angle_x)
+#     angle_z = atan2(s1*R[2, 0] - c1*R[1, 0], c1*R[1, 1] - s1*R[2, 1])
+#     return [
+#         rad_to_deg(angle_y),
+#         rad_to_deg(angle_x),
+#         rad_to_deg(angle_z)
+#     ]
+
+def calculate_angles(R: np.ndarray) -> list:
+    return [
+        rad_to_deg(atan(-R[2][0] / ((R[2][1] ** 2 + R[2][2] ** 2) ** 0.5))),
+        rad_to_deg(atan(R[2][1] / R[2][2])),
+        rad_to_deg(atan(R[1][0] / R[0][0]))
+    ]
+
+
 def calculate_angles_from_rotation_matrix(R: np.ndarray) -> list:
     return [
         rad_to_deg(atan(R[2][1] / R[2][2])),
         rad_to_deg(atan(-R[2][0] / ((R[2][1] ** 2 + R[2][2] ** 2) ** 0.5))),
         rad_to_deg(atan(R[1][0] / R[0][0]))
     ]
+
+
+def get_abs_diff(angles_1: list, angles_2: list) -> float:
+    return sum([(a1 - a2)**2 for a1, a2 in zip(angles_1, angles_2)])
+
+
+def l2r(R: np.ndarray) -> np.ndarray:
+    rotation_matrix = R.copy()
+    rotation_matrix[0, 2] *= -1
+    rotation_matrix[1, 2] *= -1
+    rotation_matrix[2, 0] *= -1
+    rotation_matrix[2, 1] *= -1
+    return rotation_matrix
 
 
 def main():
@@ -36,8 +71,8 @@ def main():
     camera_position[2] = 300
     camera = Camera([600, 600], 1, [deg_to_rad(60), deg_to_rad(60)], camera_position.copy())
 
-    bottom_left_generation_corner = [-1200, -1200]
-    height, width = 2400, 2400
+    bottom_left_generation_corner = [-800, -800]
+    height, width = 1600, 1600
     kp1 = np.ndarray([3, 1])
     kp1[0] = bottom_left_generation_corner[0]
     kp1[1] = bottom_left_generation_corner[1]
@@ -63,7 +98,7 @@ def main():
         [0.00000000e+00, 0.00000000e+00, 1.00000000e+00]
     ])
 
-    generated_keypoints = generate_keypoints_equal(kp1, kp3, x_keypoint_amount=60, y_keypoint_amount=60)
+    generated_keypoints = generate_keypoints_equal(kp1, kp3, x_keypoint_amount=30, y_keypoint_amount=30)
 
     zero_rotation_matrix = calculate_rotation_matrix(0, 0, 0)
     real_angles = []
@@ -75,7 +110,7 @@ def main():
     keypoint_current = []
     mask_current = []
 
-    x = np.linspace(0, pi*6, 100, endpoint=False)
+    x = np.linspace(0, pi*6, 50, endpoint=False)
     y_1 = sin(x[:-1])*10
     y_2 = sin(x[1:])*10
     displacement_vector = np.ndarray([3, 1])
@@ -83,24 +118,24 @@ def main():
     displacement_vector[1] = 0
     displacement_vector[2] = 0
     result_rotation_matrix = np.eye(3)
+    calculated_rotation_matrix = np.eye(3)
+    tmp_rotation_matrix_1 = np.eye(3)
+    tmp_rotation_matrix_2 = np.eye(3)
     result_angles = []
     [keypoint_previous, mask_previous] = camera.calculate_keypoint_projections_to_plane(
         generated_keypoints,
         zero_rotation_matrix
     )
 
-    total_angle_real = [0]
-    total_angle_calc = [0]
-    angle_index = 0
+    angle_index = 2
 
     for angle_1, angle_2 in zip(y_1, y_2):
-        angle = -(angle_2 - angle_1)
-        total_angle_real.append(total_angle_real[-1] + angle)
+        angle = (angle_2 - angle_1)
         camera.position += displacement_vector
         camera.update_parameters()
 
-        rotation_matrix = calculate_rotation_matrix_from_euler_angles(deg_to_rad(0), deg_to_rad(angle), deg_to_rad(0))
-        result_rotation_matrix = np.matmul(result_rotation_matrix, rotation_matrix)
+        rotation_matrix = calculate_rotation_matrix_from_euler_angles(deg_to_rad(angle), deg_to_rad(0), deg_to_rad(0))
+        result_rotation_matrix = np.matmul(rotation_matrix, result_rotation_matrix)
 
         image_camera = np.ones(camera.resolution, dtype=np.uint8) * 128
 
@@ -141,41 +176,75 @@ def main():
 
         E, mask = cv2.findEssentialMat(kps_2, kps_1, camera_matrix, method=cv2.LMEDS, threshold=2)
         R1, R2, t = cv2.decomposeEssentialMat(E)
+        # R1 *= -1
+        # R2 *= -1
+        # R1 = l2r(R1)
+        # R2 = l2r(R2)
         # retval, R, t, mask = cv2.recoverPose(E, kps_1, kps_2, camera_matrix, mask=mask)
+
+        tmp_rotation_matrix_1 = np.matmul(R1, calculated_rotation_matrix)
+        tmp_rotation_matrix_2 = np.matmul(R2, calculated_rotation_matrix)
+
+        angles_current = calculate_angles(calculated_rotation_matrix)
+        angles_1 = calculate_angles(tmp_rotation_matrix_1)
+        angles_2 = calculate_angles(tmp_rotation_matrix_2)
+
+        eps_1 = get_abs_diff(angles_current, angles_1)
+        eps_2 = get_abs_diff(angles_current, angles_2)
+
+        if eps_1 < eps_2:
+            calculated_rotation_matrix = np.matmul(R1, calculated_rotation_matrix)
+        else:
+            calculated_rotation_matrix = np.matmul(R2, calculated_rotation_matrix)
 
         keypoint_previous = keypoint_current.copy()
         mask_previous = mask_current.copy()
-        real_angles.append(angle)
-        calculated_angles_1.append(calculate_angles_from_rotation_matrix(R1))
-        calculated_angles_2.append(calculate_angles_from_rotation_matrix(R2))
-        if abs(calculated_angles_1[-1][angle_index] - angle) < abs(calculated_angles_2[-1][angle_index] - angle):
-            result_angles.append(calculated_angles_1[-1][angle_index])
-        else:
-            result_angles.append(calculated_angles_2[-1][angle_index])
+        result_angles.append(calculate_angles(calculated_rotation_matrix))
 
-        total_angle_calc.append(total_angle_calc[-1] + result_angles[-1])
+        real_angles.append(calculate_angles_from_rotation_matrix(result_rotation_matrix))
+        # calculated_angles_1.append(calculate_angles_from_rotation_matrix(R1))
+        # calculated_angles_2.append(calculate_angles_from_rotation_matrix(R2))
+        # if abs(calculated_angles_1[-1][angle_index] - angle) < abs(calculated_angles_2[-1][angle_index] - angle):
+        #     result_angles.append(calculated_angles_1[-1][angle_index])
+        # else:
+        #     result_angles.append(calculated_angles_2[-1][angle_index])
 
-    plt.plot(real_angles, 'b')
+        # total_angle_calc.append(total_angle_calc[-1] + result_angles[-1])
+
     blue_patch = mpatches.Patch(color='blue', label='Действительный угол')
-    # plt.plot([angle[0] for angle in calculated_angles_1], 'r')
-    # plt.plot([angle[1] for angle in calculated_angles_1], 'g')
-    plt.plot(result_angles, 'g')
-    green_label = mpatches.Patch(color='green', label='Рассчитанный угол')
-    #plt.plot([angle[2] for angle in calculated_angles_1], 'y')
-    plt.legend(handles=[blue_patch, green_label], loc='upper right')
+    red_patch = mpatches.Patch(color='red', label='Рассчитанный угол gamma')
+    green_patch = mpatches.Patch(color='green', label='Рассчитанный угол theta')
+    purple_patch = mpatches.Patch(color='purple', label='Рассчитанный угол Psi')
+
+    plt.plot([angles[angle_index] for angles in real_angles], 'b')
+    plt.plot([angles[0] for angles in result_angles], 'r')
+    plt.plot([angles[1] for angles in result_angles], 'g')
+    plt.plot([angles[2] for angles in result_angles], 'purple')
+    plt.legend(handles=[blue_patch, red_patch, green_patch, purple_patch], loc='upper right')
     plt.show()
 
-    plt.plot(total_angle_real, 'b')
-    plt.plot(total_angle_calc, 'g')
-    blue_patch = mpatches.Patch(color='blue', label='Действительный угол')
-    green_label = mpatches.Patch(color='green', label='Рассчитанный угол')
-    plt.legend(handles=[blue_patch, green_label], loc='upper right')
+    plt.plot([real_angle[0] - angles[0] for real_angle, angles in zip(real_angles, result_angles)], 'r')
+    plt.plot([real_angle[1] - angles[1] for real_angle, angles in zip(real_angles, result_angles)], 'g')
+    plt.plot([real_angle[2] - angles[2] for real_angle, angles in zip(real_angles, result_angles)], 'purple')
+
+    red_diff_patch = mpatches.Patch(color='red', label='Отклонение от действительного угла gamma')
+    green_diff_patch = mpatches.Patch(color='green', label='Отклонение от действительного угла theta')
+    purple_patch = mpatches.Patch(color='purple', label='Отклонение от действительного угла Psi')
+    plt.legend(handles=[red_diff_patch, green_diff_patch, purple_patch], loc='upper right')
     plt.show()
 
-    plt.plot([x - y for x, y in zip(total_angle_real, total_angle_calc)], 'g')
-    plt.show()
 
-    print(total_angle_real[-1] - total_angle_calc[-1])
+    # plt.plot(total_angle_real, 'b')
+    # plt.plot(total_angle_calc, 'g')
+    # blue_patch = mpatches.Patch(color='blue', label='Действительный угол')
+    # green_label = mpatches.Patch(color='green', label='Рассчитанный угол')
+    # plt.legend(handles=[blue_patch, green_label], loc='upper right')
+    # plt.show()
+
+    # plt.plot([x - y for x, y in zip(total_angle_real, total_angle_calc)], 'g')
+    # plt.show()
+
+    # print(total_angle_real[-1] - total_angle_calc[-1])
 
     # plt.plot(real_angles, 'b')
     # # plt.plot([angle[0] for angle in calculated_angles_2], 'r')
--- algorithm_tests/utils.py
+++ algorithm_tests/utils.py
@@ -46,3 +46,32 @@ def calculate_rotation_matrix_from_euler_angles(yaw: float, pitch: float, roll: 
     rotation_matrix[2, 1] = cos(beta)*sin(gamma)
     rotation_matrix[2, 2] = cos(beta)*cos(gamma)
     return rotation_matrix
+
+
+def calculate_rotation_matrix_from_pdf(angle_x: float, angle_y: float, angle_z: float) -> np.ndarray:
+    R1 = np.eye(3)
+    R2 = np.eye(3)
+    R3 = np.eye(3)
+    c1, s1 = cos(angle_x), sin(angle_x)
+    c2, s2 = cos(angle_y), sin(angle_y)
+    c3, s3 = cos(angle_z), sin(angle_z)
+    R1[1, 1], R1[1, 2] = c1, s1
+    R1[2, 1], R1[2, 2] =-s1, c1
+    R2[0, 0], R2[0, 2] = c2,-s2
+    R2[2, 0], R2[2, 2] = s2, c2
+    R3[0, 0], R3[0, 1] = c3, s3
+    R3[1, 0], R3[1, 1] =-s3, c3
+    return np.matmul(np.matmul(R1, R2), R3)
+
+
+def calculate_rotation_matrix_euler_angles(pitch: float, roll: float, yaw: float) -> np.ndarray:
+    B = np.eye(3)
+    B[1, 1], B[1, 2] = cos(roll), sin(roll)
+    B[2, 1], B[2, 2] = -sin(roll), cos(roll)
+    C = np.eye(3)
+    C[0, 0], C[0, 2] = cos(pitch), -sin(pitch)
+    C[2, 0], C[2, 2] = sin(pitch), cos(pitch)
+    D = np.eye(3)
+    D[0, 0], D[0, 1] = cos(yaw), sin(yaw)
+    D[1, 0], D[1, 1] = -sin(yaw), cos(yaw)
+    return np.matmul(np.matmul(B, C), D)
